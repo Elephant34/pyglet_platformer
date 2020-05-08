c8f590c09951b46919d0113501f5a9eaca990201
Elephant34 <51174423+Elephant34@users.noreply.github.com>
2020-05-08 14:50:01 +0100

    Created a title label theme

---
+++ gui_assets/labels.py
@@ -0,0 +1,12 @@
+'''
+Sets up the label themes
+'''
+import glooey
+
+class Title(glooey.Label):
+
+    custom_alignment = "center"
+
+    custom_color = "#ffffff"
+    custom_font_name = "Arial"
+    custom_font_size = 50
